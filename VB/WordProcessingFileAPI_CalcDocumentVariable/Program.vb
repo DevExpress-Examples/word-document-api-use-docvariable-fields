6f1f0c0812fd345ca022fc6da66b3fc691f23759
DevExpressExampleBot <fakeEmail>
2022-01-27 12:00:59 +0300

    Source auto update [skip ci]

--- VB/WordProcessingFileAPI_CalcDocumentVariable/Program.vb
+++ VB/WordProcessingFileAPI_CalcDocumentVariable/Program.vb
@@ -1,71 +1,63 @@
-﻿Imports DevExpress.XtraRichEdit
+Imports DevExpress.XtraRichEdit
 Imports DevExpress.XtraRichEdit.API.Native
-Imports System
-Imports System.Collections.Generic
 Imports System.Diagnostics
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 
 Namespace WordProcessingFileAPI_CalcDocumentVariable
-	Friend Class Program
-		Shared Sub Main(ByVal args() As String)
-			Dim srv As New RichEditDocumentServer()
-			srv.LoadDocument("Docs\invitation.docx")
 
-			srv.Document.Fields(0).Locked = True
+    Friend Class Program
 
-			srv.Options.MailMerge.DataSource = New SampleData()
-			AddHandler srv.Document.CalculateDocumentVariable, AddressOf Document_CalculateDocumentVariable
-			Dim myMergeOptions As MailMergeOptions = srv.Document.CreateMailMergeOptions()
+        Shared Sub Main(ByVal args As String())
+            Dim srv As RichEditDocumentServer = New RichEditDocumentServer()
+            srv.LoadDocument("Docs\invitation.docx")
+            srv.Document.Fields(0).Locked = True
+            srv.Options.MailMerge.DataSource = New SampleData()
+            AddHandler srv.Document.CalculateDocumentVariable, AddressOf Document_CalculateDocumentVariable
+            Dim myMergeOptions As MailMergeOptions = srv.Document.CreateMailMergeOptions()
+            AddHandler srv.MailMergeRecordStarted, AddressOf srv_MailMergeRecordStarted
+            AddHandler srv.MailMergeRecordFinished, AddressOf srv_MailMergeRecordFinished
+            myMergeOptions.MergeMode = MergeMode.NewSection
+            srv.Document.MailMerge(myMergeOptions, "Result.docx", DocumentFormat.OpenXml)
+            Call Process.Start("Result.docx")
+        End Sub
 
-			AddHandler srv.MailMergeRecordStarted, AddressOf srv_MailMergeRecordStarted
-			AddHandler srv.MailMergeRecordFinished, AddressOf srv_MailMergeRecordFinished
+        Private Shared Sub Document_CalculateDocumentVariable(ByVal sender As Object, ByVal e As CalculateDocumentVariableEventArgs)
+            If e.Arguments.Count > 0 Then
+                Dim location As String = e.Arguments(0).Value.ToString()
+                If(Equals(location.Trim(), String.Empty)) OrElse location.Contains("<") Then
+                    e.Value = " "
+                    e.Handled = True
+                    Return
+                End If
 
-			myMergeOptions.MergeMode = MergeMode.NewSection
-			srv.Document.MailMerge(myMergeOptions, "Result.docx", DocumentFormat.OpenXml)
+                Select Case e.VariableName
+                    Case "Weather"
+                        Dim conditions As Conditions = New Conditions()
+                        conditions = Weather.GetCurrentConditions(location)
+                        e.Value = String.Format("Weather for {0}: " & Microsoft.VisualBasic.Constants.vbLf & "Conditions: {1}" & Microsoft.VisualBasic.Constants.vbLf & "Temperature (C) :{2}" & Microsoft.VisualBasic.Constants.vbLf & "Humidity: {3}" & Microsoft.VisualBasic.Constants.vbLf & "Wind: {4}" & Microsoft.VisualBasic.Constants.vbLf, location, conditions.Condition, conditions.TempC, conditions.Humidity, conditions.Wind)
+                    Case "LOCATION"
+                        If Equals(location, "DO NOT CHANGE!") Then e.Value = DocVariableValue.Current
+                    Case Else
+                        e.Value = "LOCKED FIELD UPDATED"
+                End Select
+            Else
+                e.Value = "LOCKED FIELD UPDATED"
+            End If
 
-			Process.Start("Result.docx")
-		End Sub
+            e.Handled = True
+        End Sub
 
-		Private Shared Sub Document_CalculateDocumentVariable(ByVal sender As Object, ByVal e As CalculateDocumentVariableEventArgs)
-			If e.Arguments.Count > 0 Then
-				Dim location As String = e.Arguments(0).Value.ToString()
-				If (location.Trim() = String.Empty) OrElse (location.Contains("<")) Then
-					e.Value = " "
-					e.Handled = True
-					Return
-				End If
-				Select Case e.VariableName
-					Case "Weather"
-						Dim conditions As New Conditions()
-						conditions = Weather.GetCurrentConditions(location)
-						e.Value = String.Format("Weather for {0}: " & vbLf & "Conditions: {1}" & vbLf & "Temperature (C) :{2}" & vbLf & "Humidity: {3}" & vbLf & "Wind: {4}" & vbLf, location, conditions.Condition, conditions.TempC, conditions.Humidity, conditions.Wind)
-					Case "LOCATION"
-						If location = "DO NOT CHANGE!" Then
-							e.Value = DocVariableValue.Current
-						End If
-					Case Else
-						e.Value = "LOCKED FIELD UPDATED"
-				End Select
-			Else
-				e.Value = "LOCKED FIELD UPDATED"
-			End If
-			e.Handled = True
-		End Sub
+        Private Shared Sub srv_MailMergeRecordStarted(ByVal sender As Object, ByVal e As MailMergeRecordStartedEventArgs)
+            Dim _range As DocumentRange = e.RecordDocument.InsertText(e.RecordDocument.Range.Start, String.Format("Created on {0:G}" & Microsoft.VisualBasic.Constants.vbLf & Microsoft.VisualBasic.Constants.vbLf, Date.Now))
+            Dim cp As CharacterProperties = e.RecordDocument.BeginUpdateCharacters(_range)
+            cp.FontSize = 8
+            cp.ForeColor = Color.Red
+            cp.Hidden = True
+            e.RecordDocument.EndUpdateCharacters(cp)
+        End Sub
 
-		Private Shared Sub srv_MailMergeRecordStarted(ByVal sender As Object, ByVal e As MailMergeRecordStartedEventArgs)
-				Dim _range As DocumentRange = e.RecordDocument.InsertText(e.RecordDocument.Range.Start, String.Format("Created on {0:G}" & vbLf & vbLf, DateTime.Now))
-				Dim cp As CharacterProperties = e.RecordDocument.BeginUpdateCharacters(_range)
-				cp.FontSize = 8
-				cp.ForeColor = Color.Red
-				cp.Hidden = True
-				e.RecordDocument.EndUpdateCharacters(cp)
-		End Sub
-
-			Private Shared Sub srv_MailMergeRecordFinished(ByVal sender As Object, ByVal e As MailMergeRecordFinishedEventArgs)
-				e.RecordDocument.AppendDocumentContent("Docs\bungalow.docx", DocumentFormat.OpenXml)
-			End Sub
-	End Class
+        Private Shared Sub srv_MailMergeRecordFinished(ByVal sender As Object, ByVal e As MailMergeRecordFinishedEventArgs)
+            e.RecordDocument.AppendDocumentContent("Docs\bungalow.docx", DocumentFormat.OpenXml)
+        End Sub
+    End Class
 End Namespace
--- VB/WordProcessingFileAPI_CalcDocumentVariable/Weather.vb
+++ VB/WordProcessingFileAPI_CalcDocumentVariable/Weather.vb
@@ -1,41 +1,29 @@
-﻿Imports System
 Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Xml
-Imports System.Net
-Imports System.Web
-Imports DevExpress.Utils
-Imports DevExpress.Docs.Text
 
 Namespace WordProcessingFileAPI_CalcDocumentVariable
-	Friend Class Weather
-		Public Shared weatherDic As New Dictionary(Of String, Conditions)() From {
-			{"Berlin", New Conditions With {.Condition="Partly Cloudy", .TempC="12", .Humidity="82%", .Wind="W 20km/h"}
-			},
-			{"Marseille", New Conditions With {.Condition="Clear", .TempC="14", .Humidity="67%", .Wind="N 4km/h"}
-			},
-			{"Buenos Aires", New Conditions With {.Condition="Clear", .TempC="10.4", .Humidity="53%", .Wind="NE 3.5km/h"}
-			},
-			{"London", New Conditions With {.Condition="Overcast", .TempC="11", .Humidity="82%", .Wind="S 9.3km/h"}
-			},
-			{"Tula", New Conditions With {.Condition="Mist", .TempC="0", .Humidity="93%", .Wind="ESE 7km/h"}
-			}
-		}
 
-		Public Shared Function GetCurrentConditions(ByVal location As String) As Conditions
-			Dim result As Conditions = Nothing
-			If weatherDic.TryGetValue(location, result) Then
-				Return result
-			Else
-				Return Nothing
-			End If
-		End Function
-	End Class
-	Public Class Conditions
-		Public Property Condition() As String
-		Public Property TempC() As String
-		Public Property Humidity() As String
-		Public Property Wind() As String
-	End Class
+    Friend Class Weather
+
+        Public Shared weatherDic As Dictionary(Of String, Conditions) = New Dictionary(Of String, Conditions)() From {{"Berlin", New Conditions With {.Condition = "Partly Cloudy", .TempC = "12", .Humidity = "82%", .Wind = "W 20km/h"}}, {"Marseille", New Conditions With {.Condition = "Clear", .TempC = "14", .Humidity = "67%", .Wind = "N 4km/h"}}, {"Buenos Aires", New Conditions With {.Condition = "Clear", .TempC = "10.4", .Humidity = "53%", .Wind = "NE 3.5km/h"}}, {"London", New Conditions With {.Condition = "Overcast", .TempC = "11", .Humidity = "82%", .Wind = "S 9.3km/h"}}, {"Tula", New Conditions With {.Condition = "Mist", .TempC = "0", .Humidity = "93%", .Wind = "ESE 7km/h"}}}
+
+        Public Shared Function GetCurrentConditions(ByVal location As String) As Conditions
+            Dim result As Conditions
+            If weatherDic.TryGetValue(location, result) Then
+                Return result
+            Else
+                Return Nothing
+            End If
+        End Function
+    End Class
+
+    Public Class Conditions
+
+        Public Property Condition As String
+
+        Public Property TempC As String
+
+        Public Property Humidity As String
+
+        Public Property Wind As String
+    End Class
 End Namespace
